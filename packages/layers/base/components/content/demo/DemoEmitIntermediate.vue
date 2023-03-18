2155fcaf62a6fd61451f24ad6f623a469d7e8028
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-03-18 17:42:01 -0400

    chore: add release-it for base/layer

---
+++ packages/layers/base/components/content/demo/DemoEmitIntermediate.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      uppercase: '',
+    }
+  },
+  methods: {
+    handleCustomChange(s) {
+      this.uppercase = s
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="p-4">
+    <DemoEmitIntermediateChild @custom-change="handleCustomChange" />
+    <p>Uppercase: {{ uppercase }}</p>
+  </div>
+</template>
