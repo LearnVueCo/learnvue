e8ecd7ea81e33f77a5f2b48a30cd21fd682eb7e8
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-03-25 13:20:32 -0400

    content: update basic child

--- demos/nuxt/components/articles/vue-emit-guide/BasicChild.vue
+++ demos/nuxt/components/articles/vue-emit-guide/BasicChild.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="border-blue max-w-sm mx-auto border relative rounded">
     <div
       class="absolute right-0 -top-0 rounded-bl bg-blue-600 p-1 text-sm text-white"
     >
@@ -7,9 +7,9 @@
     </div>
     <button
       class="my-4 rounded bg-green px-2 py-1 text-white"
-      @click="$emit('add', 2)"
+      @click="$emit('add', Math.random())"
     >
-      Add 2
+      Add Math.random
     </button>
   </div>
 </template>
