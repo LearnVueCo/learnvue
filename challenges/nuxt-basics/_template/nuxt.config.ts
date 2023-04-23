e49bc82b346722eca832d10db147f7a032b7bf32
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-04-23 17:11:17 -0400

    update _template nuxt.config

--- challenges/nuxt-basics/_template/nuxt.config.ts
+++ challenges/nuxt-basics/_template/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// ⚠️ if you make any changes to this file, you must rerun `npm run dev` to make sure the tests are loading properly.
+
 export default defineNuxtConfig({
   modules: ['nuxt-vitest']
 })
